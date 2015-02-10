1a30d4d1c257b580e00126f546190fc83b2beca2
Jin Dai <jindai1783@gmail.com>
2015-02-10 15:29:33 +0000

    Wrapped up in objects

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ var github = new Github({
 
 function MakersGithub(){
   this.repo = github.getRepo('makersacademy', 'course');
-
 }
 
 MakersGithub.prototype.readFileInfo = function(file) {
-
+  this.repo.read('master', file, function(err, data) {console.log(data)});
 };
 
-repo.contents('master', "https://github.com/ddemkiw/BowlingScorecard/blob/master/index.html", function(err, contents) {}, sync=true);
 
-repo.contents('master', "https://github.com/ddemkiw/BowlingScorecard", function(err, contents) {console.log(contents)}, sync);
 
-repo.read('master', 'intro.md', function(err, data) {OneToOne.translate});
+// repo.contents('master', "https://github.com/ddemkiw/BowlingScorecard/blob/master/index.html", function(err, contents) {}, sync=true);
+
+// repo.contents('master', "https://github.com/ddemkiw/BowlingScorecard", function(err, contents) {console.log(contents)}, sync);
+
+// repo.read('master', 'intro.md', function(err, data) {OneToOne.translate});
